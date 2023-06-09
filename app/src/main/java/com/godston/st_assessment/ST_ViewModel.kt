018d5a768dbceaa68b2ddef29b9f7ee033d3c3e1
TonyoGodson <59567558+TonyoGodson@users.noreply.github.com>
2023-06-09 12:11:35 +0100

    ST_ViewModel modified

--- app/src/main/java/com/godston/st_assessment/ST_ViewModel.kt
+++ app/src/main/java/com/godston/st_assessment/ST_ViewModel.kt
@@ -4,10 +4,10 @@ import androidx.lifecycle.ViewModel
 import kotlin.random.Random
 
 class ST_ViewModel : ViewModel() {
-
-     private var bankCode: String? = ""
-
-    private
+    var accountList = emptyList<AccountDataClass>().toMutableList()
+    private var bankCode: String? = null
+    private var code: String = ""
+    var total = 0
 
     fun generate9DigitsNumber(): Int {
         val random = Random(System.currentTimeMillis())
@@ -15,18 +15,45 @@ class ST_ViewModel : ViewModel() {
     }
 
     fun calculate() {
-        bankCode = "anyBankCode"
-        var result = bankCode ?: "011"
-//        val number = "$result${generate9DigitsNumber()}".toList()
-        val number = arrayOf(Integer.parseInt("$result${generate9DigitsNumber()}".toList().toString()))
-        val mFactor = arrayOf(Integer.parseInt("373373373373".toList().toString()))
+        code = if (bankCode == null) {
+            "011"
+        } else {
+            bankCode.toString()
+        }
+        val randomNo = generate9DigitsNumber()
+        val number = "$code$randomNo".toList()
+        val mFactor = 373373373373.toString().toList()
+        val list: MutableList<Int> = mutableListOf()
         println(number)
         println(mFactor)
-//        val stBCode = ""
-//        for (i in number) {
-//           for (j in mFactor){
-//              result =
-//           }
-//        }
+
+        val mappedList = number.mapIndexed { index, num -> num to mFactor.getOrNull(index) }
+        mappedList.forEach { (numA, numB) ->
+            list.add(Integer.parseInt(numA.toString()) * Integer.parseInt(numB.toString()))
+            total += Integer.parseInt(numA.toString()) * Integer.parseInt(numB.toString())
+        }
+        val randomNumber = "$randomNo"
+        val codeAndRandomNumber = "$number".filter { it.isDigit() }
+        val _mFactor = "$mFactor".filter { it.isDigit() }
+        val mappedNumber = "$list"
+        val _total = "$total"
+        val modulo = "${total % 10}"
+        val chkDgt = if (modulo == "0") modulo
+        else "${10 - (total % 10)}"
+        println("$number${10 - (total % 10)}")
+        val acctNo = if (modulo == "0") "$randomNo$modulo"
+        else "$randomNo${10 - (total % 10)}"
+        val toAdd = AccountDataClass(
+            randomNumber,
+            codeAndRandomNumber,
+            _mFactor,
+            mappedNumber,
+            _total,
+            modulo,
+            chkDgt,
+            acctNo
+        )
+        accountList.add(toAdd)
+        total = 0
     }
 }
