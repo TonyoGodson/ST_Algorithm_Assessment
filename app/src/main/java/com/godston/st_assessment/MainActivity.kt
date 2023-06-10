554b962ef633b9fb1e85a2257524c81d0421d4b9
TonyoGodson <59567558+TonyoGodson@users.noreply.github.com>
2023-06-10 20:16:58 +0100

    viewmodel initialized in main activity

--- app/src/main/java/com/godston/st_assessment/MainActivity.kt
+++ app/src/main/java/com/godston/st_assessment/MainActivity.kt
@@ -2,20 +2,24 @@ package com.godston.st_assessment // ktlint-disable package-name
 
 import android.os.Bundle
 import android.view.View
-import androidx.activity.viewModels
 import androidx.appcompat.app.AppCompatActivity
+import androidx.lifecycle.ViewModelProvider
 import androidx.recyclerview.widget.LinearLayoutManager
 import com.godston.st_assessment.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityMainBinding
-    private val viewModel: ST_ViewModel by viewModels()
+    private lateinit var viewModel: ST_ViewModel
+
+//    private val viewModel: ST_ViewModel by viewModels()
     lateinit var accountList: MutableList<AccountDataClass>
     private lateinit var accountAdapter: AccountAdapter
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        viewModel = ViewModelProvider(this).get(ST_ViewModel::class.java)
+
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
         accountList = viewModel.accountList
